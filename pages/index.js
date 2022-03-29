9e44e6b8ad210fac6f6acddca467efd75dd9d8c5
seohs <>
2022-03-29 19:54:21 +0900

    change index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,53 @@
+import { useEffect, useState } from 'react'
+
+const API_KEY = '464be67c5928e82f82d90abb413d8de2'
+
 export default function Home() {
+  const [movies, setMovies] = useState()
+
+  useEffect(() => {
+    ;(async () => {
+      const { results } = await (
+        await fetch(`
+              https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+      ).json()
+      setMovies(results)
+    })()
+  }, [])
+
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="container">
+      {!movies ? (
+        <h4>Loading...</h4>
+      ) : (
+        movies.map((cur) => (
+          <div key={cur.id} className="movie">
+            <img src={`https://image.tmdb.org/t/p/w500/${cur.poster_path}`} />
+            <h4>{cur.original_title}</h4>
+          </div>
+        ))
+      )}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   )
 }
